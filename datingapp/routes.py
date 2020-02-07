bba1c49d4cffcb639186196d47918d5c3c7dfc43
Filipa Cagalj <filipa.cagalj@gmail.com>
2020-02-07 14:11:24 +0100

    last version

--- datingapp/routes.py
+++ datingapp/routes.py
@@ -36,7 +36,7 @@ def home():
 
 
 
-@app.route("/chat", methods=['GET', 'POST'])
+'''@app.route("/chat", methods=['GET', 'POST'])
 def chat():
     if not current_user.is_authenticated:
         flash('Please login', 'danger')
@@ -46,7 +46,60 @@ def chat():
 
 @socketio.on('message')
 def handleMessage(msg):
-	send(msg, broadcast=True)
+	send(msg, broadcast=True)'''
+
+ROOMS = ["lounge", "news", "games", "coding"]
+
+
+@app.route("/chat", methods=['GET', 'POST'])
+def chat():
+
+    if not current_user.is_authenticated:
+        flash('Please login', 'danger')
+        return redirect(url_for('login'))
+
+    return render_template("chat.html", username=current_user.username, rooms=ROOMS)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+@socketio.on('incoming-msg')
+def on_message(data):
+    """Broadcast messages"""
+
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    # Set timestamp
+    time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
+
+
+@socketio.on('join')
+def on_join(data):
+    """User joins a room"""
+
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+
+    # Broadcast that new user has joined
+    send({"msg": username + " has joined the " + room + " room."}, room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    """User leaves a room"""
+
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send({"msg": username + " has left the room"}, room=room)
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
